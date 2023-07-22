69dbf1f758ba30db717f6fe205509b75a7fae93b
muzamil007 <meixuhussain.007@gmail.com>
2023-07-22 12:23:55 +0500

    profile

--- src/components/navgation.tsx
+++ src/components/navgation.tsx
@@ -35,8 +35,8 @@ const NavItem = () => {
   };
 
   return (
-    <nav className="flex justify-around my-6">
-      <div className="flex gap-10">
+    <nav className="flex dark:bg-[#0B141D] justify-around py-6 ">
+      <div className="flex items-center gap-10">
         {mainNavItems.map((item, index) => (
           <div key={item.label} className="">
             <Link href={item.url}>
@@ -62,7 +62,7 @@ const NavItem = () => {
               </div>
 
               {open && (
-                <div className="absolute right-[250px]  z-10 mt-2 w-48 origin-top-right border-1 black:border-white-500 p-5 rounded-md  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="absolute right-[250px] dark:bg-[#0B141D]  z-10 mt-2 w-48 origin-top-right border-1 dark:border-white-500 p-5 rounded-md  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   {ProfileItem.map((item, index) => (
                     <div
                       key={item.label}
